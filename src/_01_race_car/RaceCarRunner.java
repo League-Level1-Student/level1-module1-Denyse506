362d3230066f2d78676e0988a8ffc83f96a75976
38558 <38558@0338S-S-R0B6ZHP.sdcs.local>
2020-02-04 13:43:42 -0800

    Finished

--- src/_01_race_car/RaceCarRunner.java
+++ src/_01_race_car/RaceCarRunner.java
@@ -8,17 +8,24 @@ package _01_race_car;
 
 public class RaceCarRunner {
 	public static void main(String[] args) {
+		
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+	    RaceCar r= new RaceCar("BMW",5);
 		// 2. Print the RaceCar's position in the race
-
+         System.out.println(r);
 		// 3. Crash the RaceCar
-		
+         r.crash();
+		r.pit();
+       for (int i = 0; i <7; i++) {	
+		  r.overtake();
+           }
 		// 4. If the car is damaged. Bring it in for a pit stop.
-
+           
 		// 5. Help the car move into first place.
 
 	}
+
+
 }
---
+++ src/_02_cat/Kitten.java
@@ -0,0 +1,15 @@
+package _02_cat;
+
+
+
+public class Kitten {
+	public static void main(String[] args) {
+	Cat r= new  Cat("Ginger");
+   
+	
+	r.meow();
+	r.printName();
+	r.kill();
+	
+}
+}
