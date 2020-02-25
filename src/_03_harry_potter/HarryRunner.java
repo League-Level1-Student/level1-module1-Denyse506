98d0c50006ab2a3fa405a3e0edf214764372a1a6
38558 <38558@0338S-S-R0B6ZHP.sdcs.local>
2020-02-25 13:34:43 -0800

    Done

---
+++ src/_03_harry_potter/HarryRunner.java
@@ -0,0 +1,13 @@
+package _03_harry_potter;
+
+public class HarryRunner {
+  public static void main(String[] args) {
+	
+	  HarryPotter r= new HarryPotter ();
+	  r.makeInvisible(true);
+     r.spyOnSnape();	  
+     r.makeInvisible(false);
+     r.castSpell("harry");
+}
+}
+
---
+++ src/_04_popcorn/PopcornMaker.java
@@ -0,0 +1,11 @@
+package _04_popcorn;
+
+public class PopcornMaker {
+public static void main(String[] args) {
+	Microwave m = new Microwave ();
+	Popcorn p= new Popcorn("butter");
+	m.setTime(5);
+	
+	
+}
+}
